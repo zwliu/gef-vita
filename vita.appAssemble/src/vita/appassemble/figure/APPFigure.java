a3f26ab8b864a3459cbc62a4c8c4bdbbef0782ec
liuzhiwen <zwliuf@gmail.com>
2013-03-07 12:06:11 +0800

    增加图片为背景

--- vita.appAssemble/src/vita/appassemble/figure/APPFigure.java
+++ vita.appAssemble/src/vita/appassemble/figure/APPFigure.java
@@ -1,15 +1,34 @@
 package vita.appassemble.figure;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.eclipse.draw2d.AbstractBackground;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.XYLayout;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 
 public class APPFigure extends Figure {
 	
-private Label name;
+	private Label name;
+	private static Image background = null;
+	static {
+		System.out.println(new File("").getAbsolutePath());
+		File file = new File("C:/Users/lzw/git/gef-vita/vita.appAssemble/icons/alt_about.gif");
+		try {
+			ImageData data = new ImageData(new FileInputStream(file));
+			background = new Image(Display.getDefault(), data);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public APPFigure() {
 		XYLayout layout = new XYLayout();
@@ -31,6 +50,20 @@ private Label name;
 	
 	public void setLayout(Object constraint) {
 		getParent().setConstraint(this, constraint);
+	}
+	
+	@Override
+	protected void paintFigure(Graphics graphics) {
+		if (isOpaque()) {
+			if(background != null) {
+				Rectangle area = getClientArea();
+				org.eclipse.swt.graphics.Rectangle imageArea = background.getBounds();
+				graphics.drawImage(background, imageArea.x, imageArea.y, imageArea.width, imageArea.height, area.x, area.y, area.width, area.height);
+			}
+		}
+		if (getBorder() instanceof AbstractBackground)
+			((AbstractBackground) getBorder()).paintBackground(this, graphics,
+					NO_INSETS);
 	}
 	
 }
--- vita.appAssemble/src/vita/appassemble/figure/VOMFigure.java
+++ vita.appAssemble/src/vita/appassemble/figure/VOMFigure.java
@@ -1,14 +1,34 @@
 package vita.appassemble.figure;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.eclipse.draw2d.AbstractBackground;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.ToolbarLayout;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
 
 public class VOMFigure extends Figure {
 	
 	private Label name;
+	private static Image background = null;
+	static {
+		System.out.println(new File("").getAbsolutePath());
+		File file = new File("C:/Users/lzw/git/gef-vita/vita.appAssemble/icons/alt_about.gif");
+		try {
+			ImageData data = new ImageData(new FileInputStream(file));
+			background = new Image(Display.getDefault(), data);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public VOMFigure() {
 		ToolbarLayout layout = new ToolbarLayout();
@@ -28,6 +48,20 @@ public class VOMFigure extends Figure {
 	
 	public void setLayout(Object constraint) {
 		getParent().setConstraint(this, constraint);
+	}
+	
+	@Override
+	protected void paintFigure(Graphics graphics) {
+		if (isOpaque()) {
+			if(background != null) {
+				Rectangle area = getClientArea();
+				org.eclipse.swt.graphics.Rectangle imageArea = background.getBounds();
+				graphics.drawImage(background, imageArea.x, imageArea.y, imageArea.width, imageArea.height, area.x, area.y, area.width, area.height);
+			}
+		}
+		if (getBorder() instanceof AbstractBackground)
+			((AbstractBackground) getBorder()).paintBackground(this, graphics,
+					NO_INSETS);
 	}
 	
 }
