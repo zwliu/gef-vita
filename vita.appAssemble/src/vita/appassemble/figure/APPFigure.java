ce4ad62d3c5ba8b007d5bab61552c443df32f0e8
liuzhiwen <zwliuf@gmail.com>
2013-03-11 16:14:34 +0800

    修改图片路径为相对路径

--- vita.appAssemble/src/vita/appassemble/figure/APPFigure.java
+++ vita.appAssemble/src/vita/appassemble/figure/APPFigure.java
@@ -1,8 +1,5 @@
 package vita.appassemble.figure;
 
-import java.io.File;
-import java.io.FileInputStream;
-
 import org.eclipse.draw2d.AbstractBackground;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
@@ -11,29 +8,23 @@ import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.XYLayout;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import vita.appassemble.Activator;
 
 public class APPFigure extends Figure {
 	
 	private Label name;
 	private static Image background = null;
 	static {
-		System.out.println(new File("").getAbsolutePath());
-		File file = new File("C:/Users/lzw/git/gef-vita/vita.appAssemble/icons/alt_about.gif");
-		try {
-			ImageData data = new ImageData(new FileInputStream(file));
-			background = new Image(Display.getDefault(), data);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+		ImageDescriptor imageDes = AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID,"/icons/alt_about.gif");
+		background = imageDes.createImage();
 	}
 	
 	public APPFigure() {
 		XYLayout layout = new XYLayout();
-//		layout.setHorizontal(false);
-//		layout.setSpacing(5);
 		setLayoutManager(layout);
 		name = new Label();		
 		add(name);
--- vita.appAssemble/src/vita/appassemble/figure/VOMFigure.java
+++ vita.appAssemble/src/vita/appassemble/figure/VOMFigure.java
@@ -1,8 +1,5 @@
 package vita.appassemble.figure;
 
-import java.io.File;
-import java.io.FileInputStream;
-
 import org.eclipse.draw2d.AbstractBackground;
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Figure;
@@ -11,23 +8,19 @@ import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.ToolbarLayout;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import vita.appassemble.Activator;
 
 public class VOMFigure extends Figure {
 	
 	private Label name;
 	private static Image background = null;
 	static {
-		System.out.println(new File("").getAbsolutePath());
-		File file = new File("C:/Users/lzw/git/gef-vita/vita.appAssemble/icons/alt_about.gif");
-		try {
-			ImageData data = new ImageData(new FileInputStream(file));
-			background = new Image(Display.getDefault(), data);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
+		ImageDescriptor imageDes = AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID,"/icons/alt_about.gif");
+		background = imageDes.createImage();
 	}
 	
 	public VOMFigure() {
